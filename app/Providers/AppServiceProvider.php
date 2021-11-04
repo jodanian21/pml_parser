3d400ad0a5960017e3a7edf9d1075a5489817721
j041nian <jodan_ian@hotmail.com>
2021-11-04 19:26:38 +0800

    fix https errors

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (env('HTTPS_ON') == 'true') {
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
 }
